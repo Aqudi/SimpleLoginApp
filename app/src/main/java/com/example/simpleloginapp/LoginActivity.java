73f21954c562d2002f3854405f67cf7378f52911
Aqudi <gjdigj@naver.com>
2019-10-14 15:03:26 +0900

    Login Function, Login layout and Connection with Activity are complete

--- app/src/main/java/com/example/simpleloginapp/LoginActivity.java
+++ app/src/main/java/com/example/simpleloginapp/LoginActivity.java
@@ -1,16 +1,24 @@
 package com.example.simpleloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -20,63 +28,101 @@ public class LoginActivity extends AppCompatActivity {
     TextView username;
     TextView password;
 
+    Toast toast;
+    View view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
-        String errorMSG = "";
+        Button signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        Button loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // 동작 코드 삽입
-                JSONArray userArray = null;
-                try {
-                    userArray = new JSONArray(loadJSONFromAsset());
-                } catch (org.json.JSONException ex) {
-                    ex.printStackTrace();
-                }
-                // 로그인 검사
-                android.util.Log.d("loginInfo", "로그인 시도");
-                if (userArray != null) {
-                    username = (TextView) findViewById(R.id.username);
-                    password = (TextView) findViewById(R.id.password);
-                    String name = username.toString();
-                    String pass = password.toString();
-                    boolean success = false;
+
+                username = findViewById(R.id.username);
+                password = findViewById(R.id.password);
+
+                if (username.getText().toString().isEmpty()) {
+                    username.setError("Username is required!");
+                } else if (password.getText().toString().isEmpty()) {
+                    password.setError("Password is required!");
+                } else {
+                    String errorMSG = "";
+                    JSONArray userArray = null;
                     try {
-                        for (int i = 0; i < userArray.length(); i++) {
-                            JSONObject order = userArray.getJSONObject(i);
-                            if (order.getString("username").equals(name)) {
-                                if (order.getString("password").equals(pass)) {
-                                    success = true;
-                                } else {
-                                    android.util.Log.d("loginFail", "비밀번호가 다릅니다.");
-                                }
-                            }
-                        }
+                        userArray = new JSONArray(loadJSONFromAsset());
                     } catch (org.json.JSONException ex) {
                         ex.printStackTrace();
                     }
-                    if (success) {
-                        android.util.Log.d("loginSuccess", "로그인 성공");
-                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                        startActivity(intent);
+                    // 로그인 검사
+                    android.util.Log.d("loginTry", "로그인 시도");
+                    if (userArray != null) {
+                        String name = username.getText().toString();
+                        String pass = password.getText().toString();
+                        android.util.Log.d("loginTry", "시도한 아이디 : " + name + ", 시도한 비밀번호 : " + pass);
+                        boolean success = false;
+                        try {
+                            for (int i = 0; i < userArray.length(); i++) {
+                                JSONObject order = userArray.getJSONObject(i);
+                                android.util.Log.d("loginInfo", order.toString());
+                                if (order.getString("username").equals(name)) {
+                                    android.util.Log.d("loginInfo", "Id 검색 성공!");
+                                    if (order.getString("password").equals(pass)) {
+                                        success = true;
+                                        break;
+                                    } else {
+                                        errorMSG = "비밀번호가 다릅니다.";
+                                        android.util.Log.d("loginFail", errorMSG);
+                                        break;
+                                    }
+                                } else if (i == userArray.length() - 1) {
+                                    errorMSG = "찾는 아이디가 없습니다.";
+                                    android.util.Log.d("loginFail", errorMSG);
+                                }
+                            }
+                        } catch (org.json.JSONException ex) {
+                            ex.printStackTrace();
+                        }
+                        if (success) {
+                            String successMSG = "로그인 성공";
+                            android.util.Log.d("loginSuccess", successMSG);
+                            toast = Toast.makeText(getApplicationContext(), successMSG, Toast.LENGTH_LONG);
+                            view = toast.getView();
+                            view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSuccess));
+                            toast.show();
+                            resetEditTexts();
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                        }
                     } else {
-                        android.util.Log.d("loginFail", "찾는 아이디가 없습니다.");
-                        finish();
-                        overridePendingTransition(0, 0);
-                        startActivity(getIntent());
-                        overridePendingTransition(0, 0);
+                        errorMSG = "로그인 정보 없음";
+                        android.util.Log.d("loginFail", "로그인 정보 없음");
+
+                        // 화면을 깜빡이지 않고 Activity를 재시작하는 코드
+                        //finish();
+                        //overridePendingTransition(0, 0);
+                        //startActivity(getIntent());
+                        //overridePendingTransition(0, 0);
                     }
-                } else {
-                    android.util.Log.d("loginFail", "로그인 정보 없음");
-                    // 화면을 깜빡이지 않고 Activity를 재시작하는 코드
-                    finish();
-                    overridePendingTransition(0, 0);
-                    startActivity(getIntent());
-                    overridePendingTransition(0, 0);
+                    if (!errorMSG.equals("")) {
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                        toast = Toast.makeText(getApplicationContext(), errorMSG, Toast.LENGTH_LONG);
+                        view = toast.getView();
+                        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), (R.color.colorFail)));
+                        toast.show();
+                    }
+                    resetEditTexts();
                 }
             }
         });
@@ -84,18 +130,24 @@ public class LoginActivity extends AppCompatActivity {
 
     public String loadJSONFromAsset() {
         android.util.Log.d("file open", "JSON 파일 입력");
-        String json = null;
+        String json = "";
         try {
             InputStream is = getAssets().open("precious_data.json");
             int size = is.available();
             byte[] buffer = new byte[size];
-            is.read(buffer);
+            int read = is.read(buffer);
             is.close();
             json = new String(buffer, StandardCharsets.UTF_8);
+            android.util.Log.d("file content", json);
         } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
         return json;
+    }
+
+    public void resetEditTexts() {
+        username.setText("");
+        password.setText("");
     }
 }
--- app/src/main/java/com/example/simpleloginapp/SignupActivity.java
+++ app/src/main/java/com/example/simpleloginapp/SignupActivity.java
@@ -1,14 +1,40 @@
 package com.example.simpleloginapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
+    TextView username;
+    TextView password;
+    TextView passwordCheck;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_layout);
+
+        Button signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                username = (TextView) findViewById(R.id.username);
+                password = (TextView) findViewById(R.id.password);
+                passwordCheck = (TextView) findViewById(R.id.passwordCheck);
+
+                if (username.getText().toString().isEmpty()) {
+                    username.setError("Username is required!");
+                } else if (password.getText().toString().isEmpty()) {
+                    password.setError("Password is required!");
+                } else if (passwordCheck.getText().toString().isEmpty()) {
+                    passwordCheck.setError("Password Check is required!");
+                } else {
+
+                }
+            }
+        });
     }
 }
